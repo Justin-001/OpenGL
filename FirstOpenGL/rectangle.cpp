492a0b5d78d9452b1e7c5b0ba6e044b66841a1e8
justin_001 <justin_001@users.noreply.github.com>
2019-11-14 15:49:12 +0800

    1

--- FirstOpenGL/rectangle.cpp
+++ FirstOpenGL/rectangle.cpp
@@ -85,11 +85,13 @@ int main() {
 	float vertices[] = {
 		-0.5f,-0.5f, 0.0f,
 		 0.5f,-0.5f, 0.0f,
-		 0.0f, 0.5f ,0.0f,
+		 0.5f, 0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
 	};
+
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
-	
+
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -99,8 +101,16 @@ int main() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	unsigned int indices[] = {
+		0,1,3,
+		1,2,3
+	};
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	glBindVertexArray(0);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //Ïß¿òÄ£Ê½
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -110,7 +120,7 @@ int main() {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -118,6 +128,7 @@ int main() {
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	return 0;
