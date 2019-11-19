2201c65d39d34483340877ae2e36893a46e47b62
justin_001 <justin_001@users.noreply.github.com>
2019-11-19 16:34:00 +0800

    1

--- FirstOpenGL/GLSL.cpp
+++ FirstOpenGL/GLSL.cpp
@@ -92,7 +92,7 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	float firstTriangle[] = {
+	float vertices[] = {
 		-0.5f, -0.5f, 0.0f,  // left 
 		 0.5f, -0.5f, 0.0f,  // right
 		 0.0f,  0.5f, 0.0f,  // top 
@@ -104,7 +104,7 @@ int main() {
 	
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
