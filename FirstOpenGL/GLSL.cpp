91ce6a4b6cc2ad18a6ba259297d067a46c355aa4
justin_001 <justin_001@users.noreply.github.com>
2019-11-19 16:49:55 +0800

    1

--- FirstOpenGL/GLSL.cpp
+++ FirstOpenGL/GLSL.cpp
@@ -7,25 +7,22 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+
+	"out vec3 ourColor;\n"
+
 	"void main()\n"
 	"{\n"
 	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"	ourColor = aColor;\n"
 	"}\0";
-
-/*const char* fragmentShader1Source = "#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"void main()\n"
-	"{\n"
-	"	FragColor = vec4(0.5, 0.8, 0.8, 1.0);\n"
-	"}\0";
-	*/
 
 const char* fragmentShader1Source = "#version 330 core\n"
 	"out vec4 FragColor;\n"
-	"uniform vec4 ourColor;\n"
+	"in vec3 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"	FragColor = ourColor;\n"
+	"	FragColor = vec4(ourColor, 1.0);\n"
 	"}\0";
 
 void processInput(GLFWwindow* window);
@@ -93,10 +90,12 @@ int main() {
 	glDeleteShader(fragmentShader);
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,  // left 
-		 0.5f, -0.5f, 0.0f,  // right
-		 0.0f,  0.5f, 0.0f,  // top 
+		//position           //color
+		-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,// left 
+		 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,// right
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f // top 
 	};
+
 	
 	unsigned int VAO,VBO;
 	glGenBuffers(1, &VBO);
@@ -105,8 +104,10 @@ int main() {
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
