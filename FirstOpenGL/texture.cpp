80bce6a1c1f12482244ffdc906d07c0c72b11bf2
justin_001 <justin_001@users.noreply.github.com>
2019-12-06 16:19:20 +0800

    texture

--- FirstOpenGL/texture.cpp
+++ FirstOpenGL/texture.cpp
@@ -1,7 +1,9 @@
-#include "Includes/shader.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "Includes/shader.h"
+
 #include <iostream>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "Includes/stb_image.h"
 
@@ -35,29 +37,57 @@ int main() {
 	
 	Shader ourShader("vertexShader.vert", "fragmentShader.frag");
 
+	//texture
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 
 	float vertices[] = {
-		//position           //color
-		-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,// left 
-		 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,// right
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f // top 
+		//position           //color            //texture
+		 0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f, // 右上
+		 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f, // 右下
+		-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, // 左下
+		-0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f  // 左上
 	};
 
-	
-	unsigned int VAO,VBO;
+	unsigned int indices[] = {
+		0, 1, 3,  //first triangle
+		1, 2, 3   //second triangle
+	};
+
+	unsigned int VAO, VBO, EBO;
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
-	
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -67,8 +97,9 @@ int main() {
 
 		ourShader.use();
 //		ourShader.setFloat("hoffset", 0.5f);
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
