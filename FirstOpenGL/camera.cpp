693c53dc53b0b45391775fa755297c28a9f6abff
Justin Zhang <justin_001@outlook.com>
2020-01-06 16:22:29 +0800

    FPS Camera

--- FirstOpenGL/camera.cpp
+++ FirstOpenGL/camera.cpp
@@ -245,6 +245,7 @@ void processInput(GLFWwindow* window)
 		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	cameraPos.y = 0.0f;  //FPS camera mode
 
 }
 
