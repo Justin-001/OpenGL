b3b055aa2b67f095f0efaca70120b53a17689897
Justin Zhang <justin_001@outlook.com>
2020-01-06 10:46:57 +0800

    3 circling cubes

--- FirstOpenGL/coordinate.cpp
+++ FirstOpenGL/coordinate.cpp
@@ -169,10 +169,8 @@ int main() {
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		glm::mat4 model = glm::mat4(1.0f); //矩阵初始化为单位矩阵
-		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f); //矩阵初始化为单位矩阵
 		glm::mat4 projection = glm::mat4(1.0f);
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(30.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
@@ -185,7 +183,10 @@ int main() {
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
 			float angle = 20.0f * i;
-			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
+			if ((i+1) % 3 == 0)
+				angle = 25.0f * (float)glfwGetTime();
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			ourShader.setMat4("model", model);
 
 			glDrawArrays(GL_TRIANGLES, 0, 36);
